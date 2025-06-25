accaa4b645abe7a5413104a3d63d18804b4c070a
DavidSheltonSF <david.faria012@gmail.com>
2025-06-25 18:09:25 -0300

    add search component with styles and fake data images

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,38 @@
 import { useEffect, useState } from 'react';
+import styles from './search.module.css'
 
 export default function Search({foodData, setFoodData}) {
   const [query, setQuery] = useState('');
+  const fakeData = {
+    results: [
+      {
+        id: 0,
+        title: 'Banana cake',
+        image: `src/fakeData/image0.png`,
+      },
+      {
+        id: 1,
+        title: 'Apple cake',
+        image: `src/fakeData/image1.png`,
+      },
+    ],
+  };
   useEffect(() => {
     async function feachFood() {
       const URL = 'https://api.spoonacular.com/recipes/complexSearch';
       const apiKey = '46d6800bd2e94df7877aea78a755c5e3';
       const response = await fetch(`${URL}?apiKey=${apiKey}&query=${query}`);
       const data = (await response.json()).results
+
       console.log(data);
-      setFoodData(data);
+      setFoodData(data ? data : fakeData.results);
     }
     feachFood();
   }, [query]);
 
   return (
-    <div>
-      <input
+    <div className={styles.searchContainer}>
+      <input className={styles.input}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         type="text"
