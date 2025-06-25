3eeb6c3d099cbf7594992b70b1c6590466edb63b
DavidSheltonSF <david.faria012@gmail.com>
2025-06-25 17:21:38 -0300

    add Nav component and styles; create app.css for global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import Search from './components/Search';
 import Foods from './components/Foods';
+import Nav from './components/Nav';
+import "./app.css";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
   return (
     <>
+      <Nav/>
       <Search foodData={foodData} setFoodData={setFoodData} />
       <Foods foodData={foodData} setFoodData={setFoodData} />
     </>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,5 @@
+import styles from './nav.module.css'
+
+export default function Nav() {
+  return <nav className={styles.nav}> 🍔️ FoodRecipes </nav>;
+}
