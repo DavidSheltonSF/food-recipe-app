939e55b0d831ec277c10509305b001aa3c889a82
DavidSheltonSF <david.faria012@gmail.com>
2025-06-25 20:35:41 -0300

    refactor: restructure App component layout with Container and InnerContainer

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react';
 import Search from './components/Search';
 import Foods from './components/Foods';
 import Nav from './components/Nav';
-import "./app.css";
+import './app.css';
+import Container from './components/Container';
+import InnerContainer from './components/InnerContainer';
 
 function App() {
   const [foodData, setFoodData] = useState([]);
   return (
     <>
-      <Nav/>
+      <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <Foods foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <Foods foodData={foodData} setFoodData={setFoodData} />
+        </InnerContainer>
+      </Container>
     </>
   );
 }
