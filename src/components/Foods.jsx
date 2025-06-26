82b581d7a1f2c9f19f6ff528b53591af4300dd23
DavidSheltonSF <david.faria012@gmail.com>
2025-06-25 22:10:48 -0300

    feat: add setFoodId prop to Foods component for handling food item selection

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,7 +1,11 @@
 import FoodItem from './FoodItem';
 
-export default function Foods({ foodData, setFoodData }) {
-  return <div>
-    {foodData.map((foodItem) => <FoodItem key={foodItem.id} foodItem={foodItem}/>)}
-  </div>;
+export default function Foods({ foodData, setFoodData, setFoodId }) {
+  return (
+    <div>
+      {foodData.map((foodItem) => (
+        <FoodItem key={foodItem.id} foodItem={foodItem} setFoodId={setFoodId} />
+      ))}
+    </div>
+  );
 }
